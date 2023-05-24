e0139d9b3862d5cac04482023f6299cab57481f7
Eren Nevin <eren.m.nevin@gmail.com>
2023-05-24 09:05:59 +0300

    enhance: Added all the functionalities expected from SDK that was used in snap

--- auth.ts
+++ auth.ts
@@ -30,100 +30,104 @@ export class VCModel {
     }
 }
 
-export async function call_create_vc_self_presented(
-    host: string,
-    canisterId: string,
-    selfPresentedData: string,
-) {
-    let idlFactory = ({ IDL }) =>
-        IDL.Service({
-            create_vc_self_presented: IDL.Func([IDL.Text], [IDL.Text], []),
-        });
-    let agent = await createAgent(host, null, fetch)
-    let actor = createActor(agent, canisterId, idlFactory);
+export class AuthHandler {
+    // This is used by DApps that want user to save data as VC
+    async createSelfPresentedVCModel(
+        host: string,
+        canisterId: string,
+        selfPresentedData: string,
+    ) {
+        let idlFactory = ({ IDL }) =>
+            IDL.Service({
+                create_vc_self_presented: IDL.Func([IDL.Text], [IDL.Text], []),
+            });
+        let agent = await createAgent(host, null, fetch)
+        let actor = createActor(agent, canisterId, idlFactory);
 
-    let res = ''
-    try {
-        res = `${await actor.create_vc_self_presented(
-            selfPresentedData,
-        )}`;
-    } catch {
-    }
+        let res = ''
+        try {
+            res = `${await actor.create_vc_self_presented(
+                selfPresentedData,
+            )}`;
+        } catch {
+        }
 
-    const vc_proof_pair = res.split("\n");
+        const vc_proof_pair = res.split("\n");
 
-    const vc_raw = vc_proof_pair[0];
-    const proof_raw = vc_proof_pair[1];
+        const vc_raw = vc_proof_pair[0];
+        const proof_raw = vc_proof_pair[1];
 
-    const vc = JSON.parse(vc_raw);
-    const proof = JSON.parse(proof_raw);
+        const vc = JSON.parse(vc_raw);
+        const proof = JSON.parse(proof_raw);
 
-    const did = proof["verification_method"];
+        const did = proof["verification_method"];
 
-    const data = vc["credential_subject"]["data"];
-    const issuer = vc["issuer"];
+        const data = vc["credential_subject"]["data"];
+        const issuer = vc["issuer"];
 
-    return new VCModel(res, vc, proof, did, data, issuer);
-};
-
-
-export async function call_present_did_address(
-    host: string,
-    canisterId: string,
-    did: string,
-    contractAddress: string
-) {
-    const idlFactory = ({ IDL }) => {
-        return IDL.Service({
-            present_did_address: IDL.Func([IDL.Text, IDL.Text], [IDL.Text], []),
-        });
+        return new VCModel(res, vc, proof, did, data, issuer);
     };
-    let agent = await createAgent(host, null, fetch)
-    let actor = createActor(agent, canisterId, idlFactory);
-    let res = ''
-    try {
-        res = `${await actor.present_did_address(did, contractAddress)}`
-    } catch {
-        // Won't do anything here since the error is not related to us, its related
-        // to snap not being able to verify certificate
-    }
 
-    return res
-};
+    // This is used by DApps that want user to save data as VC
+    async informCanisterAboutDeployedVCContract(
+        host: string,
+        canisterId: string,
+        did: string,
+        contractAddress: string
+    ) {
+        const idlFactory = ({ IDL }) => {
+            return IDL.Service({
+                present_did_address: IDL.Func([IDL.Text, IDL.Text], [IDL.Text], []),
+            });
+        };
+        let agent = await createAgent(host, null, fetch)
+        let actor = createActor(agent, canisterId, idlFactory);
+        let res = ''
+        try {
+            res = `${await actor.present_did_address(did, contractAddress)}`
+        } catch {
+            // Won't do anything here since the error is not related to us, its related
+            // to snap not being able to verify certificate
+        }
 
-export async function call_get_vc(
-    host: string,
-    canisterId: string,
-    did: string,
-    dapp_public: string,
-    proxy_public: string,
-): Promise<string> {
-    let idlFactory = ({ IDL }) => {
-        return IDL.Service({
-            get_vc: IDL.Func(
-                [IDL.Text, IDL.Text, IDL.Text],
-                [IDL.Text],
-                [],
-            ),
-        });
+        return res
     };
-    let agent = await createAgent(host, null, fetch)
-    let actor = createActor(agent, canisterId, idlFactory);
 
-    let res = ''
+    // This is used by DApps that only want to ask data to authenticate users
+    async askVCFromCanister(
+        host: string,
+        canisterId: string,
+        did: string,
+        dapp_public: string,
+        proxy_public: string,
+    ): Promise<string> {
+        let idlFactory = ({ IDL }) => {
+            return IDL.Service({
+                get_vc: IDL.Func(
+                    [IDL.Text, IDL.Text, IDL.Text],
+                    [IDL.Text],
+                    [],
+                ),
+            });
+        };
+        let agent = await createAgent(host, null, fetch)
+        let actor = createActor(agent, canisterId, idlFactory);
 
-    try {
-        res = `${await actor.get_vc(
-            did,
-            dapp_public,
-            proxy_public,
-        )}`;
-    } catch {
-        // Won't do anything here since the error is not related to us, its related
-        // to snap not being able to verify certificate
+        let res = ''
+
+        try {
+            res = `${await actor.get_vc(
+                did,
+                dapp_public,
+                proxy_public,
+            )}`;
+        } catch {
+            // Won't do anything here since the error is not related to us, its related
+            // to snap not being able to verify certificate
+        }
+
+        return res;
     }
 
-    return res;
 }
-
 
--- evm_handler.ts
+++ evm_handler.ts
@@ -1,99 +1,130 @@
-import { BrowserProvider, JsonRpcSigner, Contract } from "ethers";
+import { BrowserProvider, JsonRpcSigner, Contract, ContractFactory } from "ethers";
 export { parseUnits, Contract } from "ethers";
+
 import ERC20 from "./ERC20.json";
+import { VCSmartContractABI, VCSmartContractByteCode } from "./VC_SmartContracter_Bin";
+
 
 export enum ContractType {
-  ERC20,
-  ERC721,
+    ERC20,
+    ERC721,
 }
 
 export class Web3Handler {
-  provider: BrowserProvider;
-  signer: JsonRpcSigner;
-  // TODO: Type this to JsonRpcProvider
-  injectedProvider: any;
+    provider: BrowserProvider;
+    signer: JsonRpcSigner;
+    // TODO: Type this to JsonRpcProvider
+    injectedProvider: any;
 
-  constructor(injectedProvider) {
-    if (injectedProvider === undefined) {
-      this.injectedProvider = window.ethereum;
+    constructor(injectedProvider) {
+        if (injectedProvider === undefined) {
+            this.injectedProvider = window.ethereum;
+        }
     }
-  }
 
-  initialize = async () => {
-    this.provider = new BrowserProvider(this.injectedProvider);
-    this.signer = await this.provider.getSigner();
-  };
+    initialize = async () => {
+        this.provider = new BrowserProvider(this.injectedProvider);
+        this.signer = await this.provider.getSigner();
+    };
 
-  promptMetaMaskChooseWallet = async () => {
-    const permissions = await window.ethereum.request({
-      method: "wallet_requestPermissions",
-      params: [
-        {
-          eth_accounts: {},
-        },
-      ],
-    });
-    // TODO: Check permissions
+    promptMetaMaskChooseWallet = async () => {
+        const permissions = await window.ethereum.request({
+            method: "wallet_requestPermissions",
+            params: [
+                {
+                    eth_accounts: {},
+                },
+            ],
+        });
+        // TODO: Check permissions
 
-    let res = await window.ethereum.request({ method: "eth_requestAccounts" });
+        let res = await window.ethereum.request({ method: "eth_requestAccounts" });
 
-    if (res.length === 0) {
-      return "Either metamask is locked or user rejected choosing";
-    } else {
-      return res[0];
+        if (res.length === 0) {
+            return "Either metamask is locked or user rejected choosing";
+        } else {
+            return res[0];
+        }
+    };
+
+    setOnMetaMaskWalletChangeHandler = (
+        handleAccountsChanged: (accounts: string[]) => void
+    ) => {
+        window.ethereum.on("accountsChanged", handleAccountsChanged);
+    };
+
+    getAccountAddress = async () => {
+        return await this.signer.getAddress();
+    };
+    getContract(contractType: ContractType, contractAddress: string): Contract {
+        let abi = ERC20.abi;
+        if (contractType == ContractType.ERC721) {
+            // abi = ERC721.abi;
+        }
+        return new Contract(contractAddress, abi, this.signer);
     }
-  };
 
-  setOnMetaMaskWalletChangeHandler = (
-    handleAccountsChanged: (accounts: string[]) => void
-  ) => {
-    window.ethereum.on("accountsChanged", handleAccountsChanged);
-  };
+    getERC20Symbol = async (contract: Contract) => {
+        return await contract.symbol();
+    };
 
-  getAccountAddress = async () => {
-    return await this.signer.getAddress();
-  };
-  getContract(contractType: ContractType, contractAddress: string): Contract {
-    let abi = ERC20.abi;
-    if (contractType == ContractType.ERC721) {
-      // abi = ERC721.abi;
-    }
-    return new Contract(contractAddress, abi, this.signer);
-  }
+    getERC20Balance = async (contract: Contract, account: string) => {
+        console.log(`Getting balance for ${account}`);
+        return await contract.balanceOf(account);
+    };
 
-  getERC20Symbol = async (contract: Contract) => {
-    return await contract.symbol();
-  };
-
-  getERC20Balance = async (contract: Contract, account: string) => {
-    console.log(`Getting balance for ${account}`);
-    return await contract.balanceOf(account);
-  };
-
-  // Note that you need to await on the return value which is transaction
-  // receivpt
-  // await tx.wait();
-  transferERC20 = async (
-    contract: Contract,
-    toAddress: string,
-    amount: bigint
-  ) => {
-    console.log(`Transfering ${amount}
+    // Note that you need to await on the return value which is transaction
+    // receivpt
+    // await tx.wait();
+    transferERC20 = async (
+        contract: Contract,
+        toAddress: string,
+        amount: bigint
+    ) => {
+        console.log(`Transfering ${amount}
                 of ${await contract.symbol()}
                 from ${await this.getAccountAddress()}
                 to ${toAddress}`);
-    return await contract.transfer(toAddress, amount);
-  };
+        return await contract.transfer(toAddress, amount);
+    };
 
-  approveTransferERC20 = async (
-    contract: Contract,
-    toAddress: string,
-    amount: bigint
-  ) => {
-    console.log(`Approving ${amount}
+    approveTransferERC20 = async (
+        contract: Contract,
+        toAddress: string,
+        amount: bigint
+    ) => {
+        console.log(`Approving ${amount}
                 of ${await contract.symbol()}
                 from ${await this.getAccountAddress()}
                 to ${toAddress}`);
-    return await contract.approve(toAddress, amount);
-  };
+        return await contract.approve(toAddress, amount);
+    };
+
+    deployContract = async (
+        abi: string,
+        byteCode: string
+    ) => {
+        const myContractFactory = new ContractFactory(abi, byteCode, this.signer)
+        const contract = await myContractFactory.deploy();
+        return contract
+    }
+
+    deployVCContract = async (
+        id_value: string,
+        data_value: string,
+        issuer_value: string,
+        subject_value: string,
+    ) => {
+        const deployedContract = await this.deployContract('', '');
+
+        const tx = await deployedContract.setData(
+            id_value, data_value, issuer_value, subject_value
+        );
+
+        await tx.wait();
+
+        return deployedContract;
+
+    }
+
 }
