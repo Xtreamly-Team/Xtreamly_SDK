bfc9fea82b65cf1d9a6d684367ee6251cbbc6b51
Eren Nevin <eren.m.nevin@gmail.com>
2023-05-27 09:02:07 +0300

    add: Added a ProxyAccount class which encapsulate token and public address. Also added a map of proxy accounts to ProxyHandler, in case we want to support multiple proxy accounts in near future.

--- proxy.ts
+++ proxy.ts
@@ -3,11 +3,19 @@ import { createAgent, createActor, HttpAgent } from "./icp_utilities";
 // TODO: Fix calling update functions error in snap. The cause is probably bls.js file in which
 // WebAssembly is used
 // CertificateVerificationError
+//
+
+export class ProxyAccount {
+    token: string;
+    address: string;
+    constructor(token: string, address: string) {
+        this.token = token;
+        this.address = address;
+    }
+}
 
 export class ProxyHandler {
-    proxyToken: string = "";
-    proxyPublicKey: string = "";
-
+    proxyAccounts: Map<string, ProxyAccount>;
     host: string = "";
     is_snap = false;
     agent: HttpAgent | null = null;
@@ -15,6 +23,7 @@ export class ProxyHandler {
     constructor(host: string, is_snap: boolean = false) {
         this.host = host;
         this.is_snap = is_snap;
+        this.proxyAccounts = new Map();
     }
 
     // You won't need to use the returned HTTPAgent.
@@ -30,7 +39,7 @@ export class ProxyHandler {
     callCanisterCreateProxyAccount = async (
         canisterId: string,
         publicKey: string
-    ): Promise<[string, string]> => {
+    ): Promise<ProxyAccount | string> => {
         console.log(`Sending create proxy account request for
   ${publicKey}`);
         let idlFactory = ({ IDL }) => {
@@ -43,13 +52,16 @@ export class ProxyHandler {
         try {
             let res = (await actor.create_new_proxy_account(publicKey)) as stirng;
 
+            console.log(`Returned response: ${res}`);
             let [receivedToken, receivedPublicKey] = (res as string).split(",");
-            this.proxyToken = receivedToken || '';
-            this.proxyPublicKey = receivedPublicKey ? `0x${this.proxyPublicKey}` : '';
-            console.log(`Returned response: 
-                        public key: ${this.proxyPublicKey},
-                    token: ${this.proxyToken}`);
-            return [this.proxyToken, this.proxyPublicKey];
+            let proxyToken = receivedToken || '';
+            let proxyPublicKey = receivedPublicKey ? `0x${this.proxyPublicKey}` : '';
+            if (this.proxyToken && this.proxyPublicKey) {
+                let proxyAccount = new ProxyAccount(this.proxyToken, this.proxyPublicKey);
+                this.proxyAccounts.set(proxyAccount.token, proxyAccount);
+                return proxyAccount;
+            }
+            return '';
         } catch (e) {
             console.error(e);
             throw e;
